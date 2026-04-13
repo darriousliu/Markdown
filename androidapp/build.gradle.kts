b3c5455039446eaeb6dd44b497a595ff7cc977b0
Darrious Liu <60057825+darriousliu@users.noreply.github.com>
2026-04-13 20:02:51 +0800

    feat(build): 添加Hotswan编译器和预览库支持
    - 更新build.gradle.kts以引入Hotswan编译器插件
    - 在libs.versions.toml中添加Hotswan库的版本信息
    - 更新.gitignore以排除Hotswan相关文件

--- androidapp/build.gradle.kts
+++ androidapp/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.hotswan.compiler)
 }
 
 android {
@@ -52,6 +53,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    debugImplementation(libs.hotswan.preview)
 
     implementation(projects.markdownParser)
 }
